0e6f9985b82a8dff6f2cb462180f5b3296aa6711
js6pak <kubastaron@hotmail.com>
2022-10-18 19:38:16 +0200

    Replace Dummy.dll with a dummy project

---
+++ TestLibrary/InternalClass.cs
@@ -0,0 +1,25 @@
+﻿namespace TestLibrary;
+
+internal class InternalClass
+{
+    private void PrivateMethod()
+    {
+        Console.WriteLine("InternalClass.PrivateMethod");
+    }
+
+    private int PrivateProperty => 1;
+
+    private int PrivateAutoProperty { get; set; }
+}
+
+public class SecondClass
+{
+    private int _field;
+
+    private int Property { get; set; }
+
+    private int Method()
+    {
+        return 0;
+    }
+}
--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,1 +1,7 @@
-﻿new Dummy.InternalClass().PrivateMethod();
+﻿using TestLibrary;
+
+var internalClass = new InternalClass();
+internalClass.PrivateMethod();
+Console.WriteLine(internalClass.PrivateProperty);
+internalClass.PrivateAutoProperty = 123;
+Console.WriteLine(internalClass.PrivateAutoProperty);
