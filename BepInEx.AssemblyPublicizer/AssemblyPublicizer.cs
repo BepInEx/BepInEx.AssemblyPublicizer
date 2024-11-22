6f8380466b611e993b235c93f20f0b71705d9509
thegu5 <58223632+thegu5@users.noreply.github.com>
2024-11-22 15:33:01 -0500

    Update to AsmResolver 6 (#17)
    https://github.com/Washi1337/AsmResolver/releases/tag/v6.0.0-beta.1
    (cherry picked from commit 5a41adba521beb3b93880d3cfb4a0a5494314234)

--- BepInEx.AssemblyPublicizer/AssemblyPublicizer.cs
+++ BepInEx.AssemblyPublicizer/AssemblyPublicizer.cs
@@ -5,7 +5,7 @@ using AsmResolver;
 using AsmResolver.DotNet;
 using AsmResolver.DotNet.Code.Cil;
 using AsmResolver.PE.DotNet.Cil;
-using AsmResolver.PE.DotNet.Metadata.Tables.Rows;
+using AsmResolver.PE.DotNet.Metadata.Tables;
 
 namespace BepInEx.AssemblyPublicizer;
 
