d4748355ce6ed20c330f97063aad8a03c5b17471
AraHaan <seandhunt_7@yahoo.com>
2026-05-14 22:16:07 -0400

    Update BepInEx.AssemblyPublicizer.MSBuild/Extensions.cs
    Co-authored-by: Joe Clack <28568841+Lordfirespeed@users.noreply.github.com>

--- BepInEx.AssemblyPublicizer.MSBuild/Extensions.cs
+++ BepInEx.AssemblyPublicizer.MSBuild/Extensions.cs
@@ -23,13 +23,6 @@ internal static class Extensions
             return false;
         }
 
-        // Fix a problem where when one tries to publicize PhotonRealtime, PhotonUnityNetworking,
-        // and PhotonUnityNetworking.Utilities from the R.E.P.O.GameLibs.Steam nuget package.
-        // Why is this needed? Because there are mods such as LateJoin that needs access to
-        // internal fields/methods to these Proton assemblies and avoiding reflection helps solve
-        // 100% of problems related to modding R.E.P.O. while not sacrificing performance.
-        // Also the fact that Reflection sucks and way too many bugs can happen with it.
-        // Problem seems to have existed from 0.4.1 according to git blame.
         metadata = taskItem.GetMetadata(metadataName);
         if (!string.IsNullOrWhiteSpace(metadata))
         {
